11f28f38901be4b4207ad0af3d417d53fe19b65a
d-kovaleva <kovaleva.dasha98@mail.ru>
2025-01-19 22:42:42 +0400

    магазин

---
+++ src/main/java/org/github/dkovaleva/fishbowl/Decor.java
@@ -0,0 +1,16 @@
+package org.github.dkovaleva.fishbowl;
+
+public class Decor {
+    private String name;
+    private int value;
+    private String material;
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/org/github/dkovaleva/fishbowl/Fish.java
@@ -0,0 +1,15 @@
+package org.github.dkovaleva.fishbowl;
+
+public class Fish {
+    private String name;
+    private String color;
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+
+    }
+}
