38a08d511de2c5aa27d2e7eb68e5a7345ff5984c
d-kovaleva <kovaleva.dasha98@mail.ru>
2025-04-04 21:32:51 +0400

    хомеворке

---
+++ src/main/java/org/github/dkovaleva/timus/Task1545.java
@@ -0,0 +1,27 @@
+package org.github.dkovaleva.timus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task1545 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        List<String> s = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String a = scan.nextLine();
+            s.add(a);
+        }
+        String x = scan.nextLine();
+        for (int i = 0; i < n; i++) {
+            if (s.get(i).charAt(0) == x.charAt(0)) {
+                System.out.println(s.get(i));
+            }
+        }
+
+
+    }
+}
+
