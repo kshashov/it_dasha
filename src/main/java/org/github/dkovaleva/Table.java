03ed7bb116d25c8006e9cb3a490c2690c215ead3
d-kovaleva <kovaleva.dasha98@mail.ru>
2025-04-09 21:34:58 +0400

    table

--- src/main/java/org/github/dkovaleva/Table.java
+++ src/main/java/org/github/dkovaleva/Table.java
@@ -2,9 +2,19 @@ package org.github.dkovaleva;
 
 public class Table {
     public static void main(String[] args) {
+//        for (int i = 1; i <= 9; i++) {
+//            for (int j = 1; j <= 10; j++)
+//                System.out.println(i + " * " + j + " = " + (i * j));
+//        }
+        String result = "";
+
         for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 10; j++)
-                System.out.println(i + " * " + j + " = " + (i * j));
+            for (int j = 1; j <= 9; j++) {
+                result = result + j * i + " ";
+//                System.out.print(j * i + " ");
+            }
+            System.out.println(result);
+            result = "";
         }
 
 
