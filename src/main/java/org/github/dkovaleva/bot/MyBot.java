8752c937b94259f4045cd831289aa6370df06dc6
kshashov <kirill.shashov@gmail.com>
2025-06-30 20:30:03 +0400

    start todo bot

--- src/main/java/org/github/dkovaleva/bot/MyBot.java
+++ src/main/java/org/github/dkovaleva/bot/MyBot.java
@@ -8,6 +8,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
     https://github.com/rubenlagus/TelegramBots?tab=readme-ov-file
     https://rubenlagus.github.io/TelegramBotsDocumentation/telegram-bots.html
@@ -15,20 +18,55 @@ import org.telegram.telegrambots.meta.generics.TelegramClient;
 public class MyBot implements LongPollingSingleThreadUpdateConsumer {
     public static final String BOT_TOKEN = "5251321850:AAFuuXMwQzf9Hav9IxfwuRS49e_icrFkcK0";
     private TelegramClient telegramClient = new OkHttpTelegramClient(BOT_TOKEN);
+    private List<TODO> todoList = new ArrayList<>();
+
 
     @Override
     public void consume(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
+
             // Create your send message object
             String chatId = update.getMessage().getChatId().toString();
             String text = update.getMessage().getText();
 
+            if (text.startsWith("/список")) {
+                if (todoList.isEmpty()) {
+                    // если список пуст
+                    execute(new SendMessage(chatId, "Пусто"));
+                } else {
+                    // если есть задачи - формируем одну строку из задач
+                    String todos = "";
+                    int i = 1;
+                    for (TODO todo : todoList) {
+                        todos += i + todo.getText() + "\n";
+                        i++;
+                    }
+
+                    // вариант с обычным фором со счетчиком
+//                    for (int i = 0; i < todoList.size(); i++) {
+//                        todos += (i + 1) + todoList.get(i).getText() + "\n";
+//                    }
+
+                    SendMessage sendMessage = new SendMessage(chatId, todos);
+                    execute(sendMessage);
+                }
+            } else if (text.startsWith("/добавить")) {
+                // делим исходную строку пробелом чтобы отсечь команду /добавить
+                String[] todoArr = text.split(" ", 2);
+                // добавляем новую в конец списка
+                TODO todo = new TODO(todoArr[1]);
+                todoList.add(todo);
+            } else if (text.startsWith("/удалить")) {
+                // делим исходную строку пробелом чтобы отсечь команду /удалить
+                String[] todoArr = text.split(" ", 2);
+                int i = Integer.parseInt(todoArr[1]);
+                // удаляем из списка по номеру
+                todoList.remove(i - 1);
+
+            }
 
             // TODO Dasha
-
-            SendMessage sendMessage = new SendMessage(chatId, text + " hui");
-            execute(sendMessage);
         }
     }
 
---
+++ src/main/java/org/github/dkovaleva/bot/TODO.java
@@ -0,0 +1,17 @@
+package org.github.dkovaleva.bot;
+
+public class TODO {
+    private String text;
+
+    public TODO(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
